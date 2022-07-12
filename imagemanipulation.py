ec3f3a1e9472822812f3ee73d74cf04514eee638
Joshua Joshua George <76731561+joshuahey@users.noreply.github.com>
2022-07-12 12:04:13 -0600

    Update imagemanipulation.py

--- imagemanipulation.py
+++ imagemanipulation.py
@@ -3,8 +3,94 @@ import cv2
 import numpy as np
 
 
+# Copying images
 
 baboon = cv2.imread("baboon.png")
 plt.figure(figsize=(10,10))
 plt.imshow(cv2.cvtColor(baboon, cv2.COLOR_BGR2RGB))
+plt.show()
+B = baboon.copy()
+
+plt.figure(figsize=(10,10))
+plt.subplot(121)
+plt.imshow(cv2.cvtColor(baboon, cv2.COLOR_BGR2RGB))
+plt.title("baboon")
+plt.subplot(122)
+plt.imshow(cv2.cvtColor(A, cv2.COLOR_BGR2RGB))
+plt.title("array A")
+plt.imshow(cv2.cvtColor(B, cv2.COLOR_BGR2RGB))
+plt.title("array B")
+plt.show()
+
+
+# Flipping images
+image = cv2.imread("cat.png")
+plt.figure(figsize=(10,10))
+plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+width, height,C=image.shape
+# create empty matrix of same dimensions
+array_flip = np.zeros((width, height,C),dtype=np.uint8)
+
+for i,row in enumerate(image):
+        array_flip[width-1-i,:,:]=row
+    
+plt.figure(figsize=(5,5))
+plt.imshow(cv2.cvtColor(array_flip, cv2.COLOR_BGR2RGB))
+plt.show()
+
+# using flipcode
+for flipcode in [0,1,-1]:
+    im_flip =  cv2.flip(image,flipcode )
+    plt.imshow(cv2.cvtColor(im_flip,cv2.COLOR_BGR2RGB))
+    plt.title("flipcode: "+str(flipcode))
+    plt.show()
+# using rotate
+im_flip = cv2.rotate(image,0)
+plt.imshow(cv2.cvtColor(im_flip,cv2.COLOR_BGR2RGB))
+plt.show()
+# using Flip
+flip = {"ROTATE_90_CLOCKWISE":cv2.ROTATE_90_CLOCKWISE,"ROTATE_90_COUNTERCLOCKWISE":cv2.ROTATE_90_COUNTERCLOCKWISE,"ROTATE_180":cv2.ROTATE_180}
+for key, value in flip.items():
+    plt.subplot(1,2,1)
+    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    plt.title("orignal")
+    plt.subplot(1,2,2)
+    plt.imshow(cv2.cvtColor(cv2.rotate(image,value), cv2.COLOR_BGR2RGB))
+    plt.title(key)
+    plt.show()
+
+    
+    
+# Cropping Images
+array_sq = np.copy(image)
+array_sq[upper:lower,left:right,:] = 0
+
+plt.figure(figsize=(10,10))
+plt.subplot(1,2,1)
+plt.imshow(cv2.cvtColor(image,cv2.COLOR_BGR2RGB))
+plt.title("orignal")
+plt.subplot(1,2,2)
+plt.imshow(cv2.cvtColor(array_sq,cv2.COLOR_BGR2RGB))
+plt.title("Altered Image")
+plt.show()
+
+start_point, end_point = (left, upper),(right, lower)
+image_draw = np.copy(image)
+cv2.rectangle(image_draw, pt1=start_point, pt2=end_point, color=(0, 255, 0), thickness=3) 
+plt.figure(figsize=(5,5))
+plt.imshow(cv2.cvtColor(image_draw, cv2.COLOR_BGR2RGB))
+plt.show()
+
+# img: Image array
+# text: Text string to be overlayed
+# org: Bottom-left corner of the text string in the image
+# fontFace: tye type of font
+# fontScale: Font scale
+# color: Text color
+# thickness: Thickness of the lines used to draw a text
+# lineType: Line type
+
+image_draw=cv2.putText(img=image,text='Stuff',org=(10,500),color=(255,255,255),fontFace=4,fontScale=5,thickness=2)
+plt.figure(figsize=(10,10))
+plt.imshow(cv2.cvtColor(image_draw,cv2.COLOR_BGR2RGB))
 plt.show()

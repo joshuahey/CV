985beb802e45b9773e87d9d1b0d2bfe955cac2b1
Joshua Joshua George <76731561+joshuahey@users.noreply.github.com>
2022-07-12 12:20:58 -0600

    Update mathoperations.py

--- mathoperations.py
+++ mathoperations.py
@@ -11,3 +11,30 @@ def plot_image(image_1, image_2,title_1="Orignal",title_2="New Image"):
     plt.imshow(image_2,cmap="gray")
     plt.title(title_2)
     plt.show()
+
+im_gray = cv2.imread('barbara.png', cv2.IMREAD_GRAYSCALE)
+im_gray.shape
+
+plt.imshow(im_gray,cmap='gray')
+plt.show()
+
+U, s, V = np.linalg.svd(im_gray , full_matrices=True)
+S = np.zeros((im_gray.shape[0], im_gray.shape[1]))
+S[:image.shape[0], :image.shape[0]] = np.diag(s)
+
+plot_image(U,V,title_1="Matrix U ",title_2="matrix  V")
+B = S.dot(V)
+plt.imshow(B,cmap='gray')
+plt.show()
+A = U.dot(B)
+plt.imshow(A,cmap='gray')
+plt.show()
+
+# for removing redundant  elements
+for n_component in [1,10,100,200, 500]:
+    S_new = S[:, :n_component]
+    V_new = V[:n_component, :]
+    A = U.dot(S_new.dot(V_new))
+    plt.imshow(A,cmap='gray')
+    plt.title("Number of Components:"+str(n_component))
+    plt.show()
